6fc64db222a7924b03c5ee90a7db6c31f2be4058
IayZBoy <114676300+IayZBoy@users.noreply.github.com>
2025-01-05 14:14:42 +0100

    Update Rayfield.lua

--- Rayfield.lua
+++ Rayfield.lua
@@ -534,7 +534,7 @@ repeat
 		warned = true
 	end
 
-	toDestroy, Rayfield = Rayfield, useStudio and script.Parent:FindFirstChild('Rayfield') or game:GetObjects("rbxassetid://10804731440")[1]
+	toDestroy, Rayfield = Rayfield, useStudio and script.Parent:FindFirstChild('Rayfield') or game:GetObjects("rbxassetid://77649443597500")[1]
 	if toDestroy and not useStudio then toDestroy:Destroy() end
 
 	buildAttempts = buildAttempts + 1
